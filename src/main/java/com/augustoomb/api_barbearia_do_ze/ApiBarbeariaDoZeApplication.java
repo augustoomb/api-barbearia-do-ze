cae39505aa4afac1716988d7089381416186ae48
Augusto Barbosa <augustobarbosa1988@gmail.com>
2026-08-31 11:40:24 -0300

    first commit

---
+++ src/main/java/com/augustoomb/api_barbearia_do_ze/ApiBarbeariaDoZeApplication.java
@@ -0,0 +1,13 @@
+package com.augustoomb.api_barbearia_do_ze;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiBarbeariaDoZeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiBarbeariaDoZeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/augustoomb/api_barbearia_do_ze/ApiBarbeariaDoZeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.augustoomb.api_barbearia_do_ze;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiBarbeariaDoZeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
